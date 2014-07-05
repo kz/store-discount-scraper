871fdb744d0aff18b384824f33381eee670d1803
Kelvin Z <hello@iamkelv.in>
2014-07-05 17:43:52 +0100

    Add sale page scraping to mod_madecom

--- mod_madecom.py
+++ mod_madecom.py
@@ -3,4 +3,7 @@ from lxml import html
 import requests
 
 def madecom():
-    return 'Hello world.'
+    base_page = requests.get('http://www.made.com/sale')
+    base_page_text = html.fromstring(base_page.text)
+    sale_pages = base_page_text.xpath('/html/body/div[3]/div[2]/div/div/div/div/div/a/@href')
+    return sale_pages
